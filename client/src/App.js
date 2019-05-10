f7604f8eab1942a4b63243c8fbe9ca9d68844e49
unknown <rmtestard@gmail.com>
2019-05-10 10:48:14 +0200

    routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 const App = () => (
-  <Fragment>
-    <Navbar />
-    <Landing />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+      <div className="container">
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+      </div>
+    </Fragment>
+  </Router>
 );
 
 export default App;
